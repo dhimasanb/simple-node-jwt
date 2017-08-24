9a343360bbc43dade3d3ffe9a0b042b72e8a6d35
Dhimas Akbar N.B <dhimasakbarnoorbaihaque@gmail.com>
2017-08-24 23:50:27 +0700

    Add route & use mongoclient

--- server.js
+++ server.js
@@ -15,9 +15,11 @@ let User = require('./app/models/user'); // get our mongoose model
 // configuration =========
 // =======================
 let port = process.env.PORT || 8080; // used to create, sign, and verify tokens
-mongoose.connect(config.database); // connect to database
+mongoose.connect(config.database, {
+    useMongoClient: true,
+    /* other options */
+}); // connect to database
 app.set('superSecret', config.secret); // secret variable
-
 // use body parser so we can get info from POST and/or URL parameters
 app.use(bodyParser.urlencoded({
     extended: false,
@@ -36,6 +38,28 @@ app.get('/', function(req, res) {
 });
 
 // API ROUTES -------------------
+// get an instance of the router for api routes
+let apiRoutes = express.Router();
+
+// TODO: route to authenticate a user (POST http://localhost:8080/api/authenticate)
+
+// TODO: route middleware to verify a token
+
+// route to show a random message (GET http://localhost:8080/api/)
+apiRoutes.get('/', function(req, res) {
+  res.json({message: 'Welcome to the coolest API on earth!'});
+});
+
+// route to return all users (GET http://localhost:8080/api/users)
+apiRoutes.get('/users', function(req, res) {
+  User.find({}, function(err, users) {
+    res.json(users);
+  });
+});
+
+// apply the routes to our application with the prefix /api
+app.use('/api', apiRoutes);
+
 // we'll get to these in a second
 app.get('/setup', function(req, res) {
     // create a sample user
