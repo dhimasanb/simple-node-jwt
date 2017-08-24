2dd119878854f2a3bcd8ffc480f219ee6362499b
Dhimas Akbar N.B <dhimasakbarnoorbaihaque@gmail.com>
2017-08-25 06:48:25 +0700

    Create route auth & token

--- server.js
+++ server.js
@@ -42,6 +42,37 @@ app.get('/', function(req, res) {
 let apiRoutes = express.Router();
 
 // TODO: route to authenticate a user (POST http://localhost:8080/api/authenticate)
+// route to authenticate a user (POST http://localhost:8080/api/authenticate)
+apiRoutes.post('/authenticate', function(req, res) {
+      // find the user
+      User.findOne({
+        name: req.body.name,
+      }, function(err, user) {
+        if (err) throw err;
+
+        if (!user) {
+          res.json({success: false, message: 'Authentication failed. User not found.'});
+        } else if (user) {
+          // check if password matches
+          if (user.password != req.body.password) {
+            res.json({success: false, message: 'Authentication failed. Wrong password.'});
+          } else {
+            // if user is found and password is right
+            // create a token
+            let token = jwt.sign(user, app.get('superSecret'), {
+                expiresIn: 1440, // expires in 24 hours
+            });
+
+            // return the information including token as JSON
+            res.json({
+              success: true,
+              message: 'Enjoy your token!',
+              token: token,
+            });
+          }
+        }
+      });
+    });
 
 // TODO: route middleware to verify a token
 
