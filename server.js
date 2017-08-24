d17708f484ee75f1296157fd90bcc79119bb6610
Dhimas Akbar N.B <dhimasakbarnoorbaihaque@gmail.com>
2017-08-25 06:56:13 +0700

    Create middleware to protect routes

--- server.js
+++ server.js
@@ -75,6 +75,31 @@ apiRoutes.post('/authenticate', function(req, res) {
     });
 
 // TODO: route middleware to verify a token
+apiRoutes.use(function(req, res, next) {
+    // check header or url parameters or post parameters for token
+    let token = req.body.token || req.query.token || req.headers['x-access-token'];
+
+    // decode token
+    if (token) {
+      // verifies secret and checks exp
+      jwt.verify(token, app.get('superSecret'), function(err, decoded) {
+        if (err) {
+          return res.json({success: false, message: 'Failed to authenticate token.'});
+        } else {
+          // if everything is good, save to request for use in other routes
+          req.decoded = decoded;
+          next();
+        }
+      });
+    } else {
+      // if there is no token
+      // return an error
+      return res.status(403).send({
+          success: false,
+          message: 'No token provided.',
+      });
+    }
+  });
 
 // route to show a random message (GET http://localhost:8080/api/)
 apiRoutes.get('/', function(req, res) {
